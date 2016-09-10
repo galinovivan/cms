5c90776c45f710d4103a50bc9e9c1f2a15227eec
galinovivan <9225913@gmail.com>
2016-09-11 00:47:35 +0300

    add article class

--- config.php
+++ config.php
@@ -1,19 +1,20 @@
 <?php
-ini_set("display_errors", true);
-date_default_timezone_set("Europe/Moscow");
-define("DB_DSN", "mysql:host=localhost;dbname=cms");
-define("DB_USERNAME", "root");
-define("DB_PASSWORD", "");
-define("CLASS_PATH", "classes");
-define("TEMPLATE_PATH", "templates");
-define("HOMEPAGE_NUM_ARTICLES", 5);
-define("ADMIN_USERNAME", "admin");
-define("ADMIN_PASSWORD", "password");
-require (CLASS_PATH . "/Article.php");
+ini_set("display_errors", true); // режим ошибок в браузере (после разрааботки отключить)
+date_default_timezone_set("Europe/Moscow"); //устанавливаем временную зону
+define("DB_DSN", "mysql:host=localhost;dbname=cms"); // путь в бд
+define("DB_USERNAME", "root"); // логин бд
+define("DB_PASSWORD", ""); // пароль к бд
+define("CLASS_PATH", "classes"); // путь к папке класса
+define("TEMPLATE_PATH", "templates"); // путь к папке с шаблонами
+define("HOMEPAGE_NUM_ARTICLES", 5); // максимум статей на главной
+define("ADMIN_USERNAME", "admin"); // логин админа
+define("ADMIN_PASSWORD", "password"); // пароль админа
+require (CLASS_PATH . "/Article.php"); // класс статей
 
+// функция обработке исключенных ситуаций
 function handleException($exception) {
     echo "Пожалуйсто попробуйте пойзже";
     error_log($exception->getMessage());
 }
-set_exception_handler('handleException');
+set_exception_handler('handleException'); // запись ошибок в лог сервера
 ?>
