fbd5ea9e46db43f0a134774c8e6637a5f049d85c
galinovivan <9225913@gmail.com>
2016-09-11 01:30:24 +0300

    add admin.php start

---
+++ admin.php
@@ -0,0 +1,31 @@
+<?php
+require ("config.php");
+session_start();
+$action = isset($_GET['action']) ? $_GET['action'] : "";
+$username = isset($_SESSION['username']) ? $_SESSION['username'] : "";
+
+if ($action != "login" && $action != "logout" && !$username) {
+    login();
+    exit();
+}
+switch ($action) {
+    case 'login':
+        login();
+        break;
+    case 'logout':
+        logout();
+        break;
+    case 'newArticle':
+        newArticle();
+        break;
+    case 'editArticle':
+        editArticle();
+        break;
+    case 'deleteArticle':
+        deleteArticle();
+        break;
+    default:
+        listArticles();
+}
+
+?>
